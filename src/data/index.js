7f9198f535cce2d2a354769d4660ba47265f4dfb
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2024-02-11 17:37:02 +0800

    refactor: externalize project data and implement map rendering
    - Externalize project data into a separate JS file for improved maintainability.
    - Utilize map function in the projects page to dynamically render Project and FeaturedProject components based on the external data.
    
    This refactor enhances code readability and scalability by separating the data layer from the presentation layer, allowing for easier updates and additions to project listings in the future.

---
+++ src/data/index.js
@@ -0,0 +1,71 @@
+import {
+  blogging_rest_api,
+  cashien,
+  chronos,
+  echoflow,
+  grocereasy,
+  metis,
+  thothes,
+} from '@/assets';
+
+export const projectsData = [
+  {
+    id: 'echoflow',
+    title: 'Echoflow',
+    type: 'Front-End Project',
+    summary: 'Echoflow is a web forum for user-generated discussions...',
+    img: echoflow,
+    link: 'https://echoflow.vercel.app/',
+    github: 'https://github.com/gifariarsal/echoflow',
+    featured: true,
+  },
+  {
+    id: 'grocereasy',
+    title: 'GrocerEasy',
+    type: 'Full-Stack Project',
+    img: grocereasy,
+    link: 'https://github.com/gifariarsal/final_project_group_1/tree/development',
+    github:
+      'https://github.com/gifariarsal/final_project_group_1/tree/development',
+  },
+  {
+    id: 'chronos',
+    title: 'Chronos',
+    type: 'Full-Stack Project',
+    img: chronos,
+    link: 'https://github.com/gifariarsal/chronos_client',
+    github: 'https://github.com/gifariarsal/chronos_client',
+  },
+  {
+    id: 'thothes',
+    title: 'Thothes',
+    type: 'Front-End Project',
+    img: thothes,
+    link: 'https://thothes.vercel.app',
+    github: 'https://github.com/gifariarsal/personal-notes-v2',
+  },
+  {
+    id: 'cashien',
+    title: 'Cashien',
+    type: 'Full-Stack Project',
+    img: cashien,
+    link: 'https://github.com/gifariarsal/group1/tree/development',
+    github: 'https://github.com/gifariarsal/group1/tree/development',
+  },
+  {
+    id: 'metis',
+    title: 'Metis',
+    type: 'Front-End Project',
+    img: metis,
+    link: 'https://github.com/gifariarsal/metis',
+    github: 'https://github.com/gifariarsal/metis',
+  },
+  {
+    id: 'blogging_rest_api',
+    title: 'Blogging REST API',
+    type: 'Back-End Project',
+    img: blogging_rest_api,
+    link: 'https://documenter.getpostman.com/view/27216835/2s946pYUD5',
+    github: 'https://github.com/gifariarsal/blogging_backend',
+  },
+];
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -14,6 +14,7 @@ import {
 } from '@/assets';
 import FeaturedProject from '@/components/FeaturedProject';
 import Project from '@/components/Project';
+import { projectsData } from '@/data';
 
 const projects = () => {
   return (
@@ -30,70 +31,17 @@ const projects = () => {
             className="mb-16 lg:!text-7xl sm:!text-6xl sm:mb-8 xs:!text-4xl"
           />
           <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-16 sm:gap-x-0">
-            <div className="col-span-12">
-              <FeaturedProject
-                title="Echoflow"
-                type="Front-End Project"
-                summary="Echoflow is a web forum for user-generated discussions, featuring thread creation, comments, and an upvote/downvote system, fostering a vibrant community for sharing ideas and interactions."
-                img={echoflow}
-                link="https://echoflow.vercel.app/"
-                github="https://github.com/gifariarsal/echoflow"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                title="GrocerEasy"
-                type="Full-Stack Project"
-                img={grocereasy}
-                link="https://github.com/gifariarsal/final_project_group_1/tree/development"
-                github="https://github.com/gifariarsal/final_project_group_1/tree/development"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                title="Chronos"
-                type="Full-Stack Project"
-                img={chronos}
-                link="https://github.com/gifariarsal/chronos_client"
-                github="https://github.com/gifariarsal/chronos_client"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                title="Thothes"
-                type="Front-End Project"
-                img={thothes}
-                link="https://thothes.vercel.app"
-                github="https://github.com/gifariarsal/personal-notes-v2"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                title="Cashien"
-                type="Full-Stack Project"
-                img={cashien}
-                link="https://github.com/gifariarsal/group1/tree/development"
-                github="https://github.com/gifariarsal/group1/tree/development"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                title="Metis"
-                type="Front-End Project"
-                img={metis}
-                link="https://github.com/gifariarsal/metis"
-                github="https://github.com/gifariarsal/metis"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                title="Blogging REST API"
-                type="Back-End Project"
-                img={blogging_rest_api}
-                link="https://documenter.getpostman.com/view/27216835/2s946pYUD5"
-                github="https://github.com/gifariarsal/blogging_backend"
-              />
-            </div>
+            {projectsData.map((project) =>
+              project.featured ? (
+                <div className="col-span-12" key={project.id}>
+                  <FeaturedProject {...project} />
+                </div>
+              ) : (
+                <div className="col-span-6 sm:col-span-12" key={project.id}>
+                  <Project {...project} />
+                </div>
+              )
+            )}
           </div>
         </Layout>
       </main>

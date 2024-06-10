600b5fb6a99b1ed4d32a2709c3de959d207d5e6d
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2024-06-10 17:56:11 +0800

    feat: convert images from PNG to WebP and adjust their sizes
    - Change image format from PNG to WebP for better performance
    - Adjust image sizes to optimize display and loading times

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,14 +1,14 @@
-import about_pic from '@/assets/images/profile/about_pic.png';
-import index_pic from '@/assets/images/profile/index_pic.png';
-import blogging_rest_api from '@/assets/images/projects/blogging_rest_api.png';
-import cashien from '@/assets/images/projects/cashien.png';
-import chronos from '@/assets/images/projects/chronos.png';
-import echoflow from '@/assets/images/projects/echoflow.png';
-import grocereasy from '@/assets/images/projects/grocereasy.png';
-import metis from '@/assets/images/projects/metis.png';
-import thothes from '@/assets/images/projects/thothes.png';
-import prestige from '@/assets/images/projects/prestige.png';
-import chromatiq from '@/assets/images/projects/chromatiq.png';
+import about_pic from '@/assets/images/profile/about_pic.webp';
+import index_pic from '@/assets/images/profile/index_pic.webp';
+import blogging_rest_api from '@/assets/images/projects/blogging_rest_api.webp';
+import cashien from '@/assets/images/projects/cashien.webp';
+import chronos from '@/assets/images/projects/chronos.webp';
+import echoflow from '@/assets/images/projects/echoflow.webp';
+import grocereasy from '@/assets/images/projects/grocereasy.webp';
+import metis from '@/assets/images/projects/metis.webp';
+import thothes from '@/assets/images/projects/thothes.webp';
+import prestige from '@/assets/images/projects/prestige.webp';
+import chromatiq from '@/assets/images/projects/chromatiq.webp';
 import logo_gifari from '@/assets/images/logo_gifari.png';
 
 export {

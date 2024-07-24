05063b7f0e3cac8703f89cc42e1b920514813549
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2024-07-24 22:16:09 +0800

    refactor: update work experience description to list format, separate data into a data file
    - Change work experience description to list format for improved readability
    - Move work experience data into a separate file for better organization and maintainability

--- src/components/about/Experience.js
+++ src/components/about/Experience.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { useScroll, motion } from "framer-motion";
 import LiIcon from "../common/LiIcon";
+import { experiences } from "@/data";
 
 const Details = ({ position, company, companyLink, time, work }) => {
-    const ref = React.useRef(null);
+  const ref = React.useRef(null);
   return (
     <li
       ref={ref}
@@ -29,18 +30,24 @@ const Details = ({ position, company, companyLink, time, work }) => {
         <p className="capitalize font-medium text-dark/75 dark:text-light/75 my-2 xs:text-sm">
           {time}
         </p>
-        <p className="font-medium w-full md:text-sm">{work}</p>
+        <ul className="list-disc pl-5 w-full md:text-sm space-y-3">
+          {work.map((item, index) => (
+            <li key={index} className="font-medium text-justify">
+              {item}
+            </li>
+          ))}
+        </ul>
       </motion.div>
     </li>
   );
 };
 
 const Experience = () => {
-    const ref = React.useRef(null);
-    const { scrollYProgress } = useScroll({
-      target: ref,
-      offset: ["start end", "center start"],
-    });
+  const ref = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center start"],
+  });
   return (
     <div className="my-40 md:my-20">
       <h2 className="w-full text-8xl font-bold text-center mb-16 md:text-6xl md:mb-8 xs:text-4xl">
@@ -52,27 +59,18 @@ const Experience = () => {
           className="absolute left-9 top-1 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]"
         />
         <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
-          <Details
-            position="Frontend Developer"
-            companyLink="https://www.chromatiq.id/"
-            company="Chromatiq"
-            time="Feb 2024 - Present"
-            work="I am responsible for developing website projects received by the agency from clients. This involves determining the design experience, coding to develop the website, and even engaging in copywriting. I support project goals by contributing to all stages of development."
-          />
-          <Details
-            position="Chemistry Tutor"
-            companyLink="https://www.quipper.com/id/"
-            company="Quipper"
-            time="Feb 2019 - Jun 2023"
-            work="I supported students by assisting with assignments, explaining complex problem-solving methods, and ensuring prompt responses to their needs. I guided them through challenges, simplified concepts, and tailored my approach for individual learning, fostering an interactive environment. My focus on effective communication and adaptability enhanced understanding of complex subjects."
-          />
-          <Details
-            position="Tutor Test Center"
-            companyLink="https://pahamify.com/"
-            company="Pahamify"
-            time="Feb 2021 - May 2021"
-            work="I focused in producing detailed problem-solving videos, delved into complex problem concepts, meticulously planned and delivered clear explanations. Collaborated closely with the Quality Control team, ensuring the videos met stringent content standards, resulting in high-quality educational materials tailored to our audience's needs."
-          />
+          {experiences.map(
+            ({ id, position, companyLink, company, time, work }) => (
+              <Details
+                key={id}
+                position={position}
+                company={company}
+                companyLink={companyLink}
+                time={time}
+                work={work}
+              />
+            )
+          )}
         </ul>
       </div>
     </div>
